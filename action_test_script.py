4ef2491fa9ff75fc470eb6b8573e7bb2e4a1f708
lastnameswayne <tiarnan1109@gmail.com>
2023-03-23 10:48:14 +0100

    run

--- action_test_script.py
+++ action_test_script.py
@@ -1,6 +1,8 @@
 #should create a string file and be able to push it
 def main():
+    print("hell")
     #create a string file
     string_file = open("string_file.txt", "w")
     string_file.write("This is a string file")
     string_file.close()
+main()
