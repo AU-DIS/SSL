a47f405fe4ee49cee445a9292b9b200cb5f48a4e
lastnameswayne <tiarnan1109@gmail.com>
2023-03-23 10:56:39 +0100

    last actions test

--- action_test_script.py
+++ action_test_script.py
@@ -5,4 +5,26 @@ def main():
     string_file = open("string_file.txt", "w")
     string_file.write("This is a string file")
     string_file.close()
+    balanced_accuracies = []
+    accuracies = []
+    recalls = []
+    precisions = []
+    f1s = []
+    for i in range(0, 10):
+        balanced_accuracies.append(i)
+        accuracies.append(i)
+        recalls.append(i)
+        precisions.append(i)
+        f1s.append(i)
+    f = open('balanced_accuracy.txt', 'a+')
+    f.write(str(balanced_accuracies))
+    f = open('accuracy.txt', 'a+')
+    f.write(str(accuracies))
+    f = open('recall.txt', 'a+')
+    f.write(str(recalls))
+    f = open('precision.txt', 'a+')
+    f.write(str(precisions))
+    f = open('f1.txt', 'a')
+    f.write(str(f1s))
+
 main()
