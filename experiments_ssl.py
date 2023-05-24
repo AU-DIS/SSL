5671c202aa0c4b54967d75bd050b98e3c7170772
lastnameswayne <tiarnan1109@gmail.com>
2023-05-24 10:06:22 +0200

    experiments_final_4

--- experiments_ssl.py
+++ experiments_ssl.py
@@ -692,7 +692,7 @@ if __name__ == '__main__':
                 ground_truth = []
 
                 script_dir = os.path.dirname(__file__)
-                rel_path = f'experiments_final_3/{graph_name}/{per}/{lr*100}'
+                rel_path = f'experiments_final_4/{graph_name}/{per}/{lr*100}'
                 Path(rel_path).mkdir(parents=True, exist_ok=True)
                 abs_file_path = os.path.join(script_dir, rel_path)
                 if folder_no == 0: 
@@ -800,7 +800,7 @@ if __name__ == '__main__':
                         # og_ged.append(og_results["graph_edit_distance"])
 
                        # Write results for standard voting 
-                rel_path = f'experiments_final_3/{graph_name}/{per}/{lr*100}'
+                rel_path = f'experiments_final_4/{graph_name}/{per}/{lr*100}'
                 Path(rel_path).mkdir(parents=True, exist_ok=True)
                 script_dir = os.path.dirname(__file__)
                 abs_file_path = os.path.join(script_dir, rel_path)
