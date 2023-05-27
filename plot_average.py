bcc23d3c903fe7bb7f64ec3837fd684fb7a6c91a
lastnameswayne <tiarnan1109@gmail.com>
2023-05-27 11:58:17 +0200

    folder_amount=5

--- plot_average.py
+++ plot_average.py
@@ -136,8 +136,8 @@ def plot(plt, conductance_list, balanced_accuracy_list, label, should_scatter = 
 
     z = np.polyfit(x, y, 2)
     p = np.poly1d(z)
-    if should_scatter:
-        plt.scatter(x, y)
+    # if should_scatter:
+    plt.scatter(x, y)
     plt.plot(x, p(x), label=label)
 
 if __name__ == '__main__':
