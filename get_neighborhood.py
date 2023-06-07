4f5be058bee1169f64bd63cb0531c61e23df0d85
casper402 <casperdamlarsen@gmail.com>
2023-06-07 16:04:44 +0200

    fixed quartic and linear data for football 0.1

--- get_neighborhood.py
+++ get_neighborhood.py
@@ -18,7 +18,7 @@ if len(sys.argv) >= 3:
     per = sys.argv[2]
 
 if __name__ == '__main__':
-    rootdir = f'experiments_final_5/{graph}/{per}'
+    rootdir = f'experiments_final/{graph}/{per}'
     directories = []
 
     for file in os.listdir(rootdir):
